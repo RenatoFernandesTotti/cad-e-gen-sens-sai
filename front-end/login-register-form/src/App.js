058b65fc674337967d0a3d37eb9edd7d53d0f0e3
Leonardosaigm <leonardosaigm@hotmail.com>
2023-06-25 23:18:17 -0300

    tudo novo acho q agora foi

---
+++ front-end/login-register-form/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import logo from './logo.svg';
+import './App.css';
+import { Login } from './login';
+import { Register } from './register';
+
+function App() {
+  const [currentForm, setCurrentForm] = useState('login')
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
+
+  return (
+    <div className="App">
+      {
+        currentForm === "login" ? <Login onFormSwitch = {toggleForm} /> : <Register onFormSwitch = {toggleForm} />
+      }
+      
+    </div>
+  );
+}
+
+export default App;
