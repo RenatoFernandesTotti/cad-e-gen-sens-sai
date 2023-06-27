3a5cdf092ed110a631d840ac300a4677b188aa4a
Renato <renatoaure@outlook.com>
2023-06-26 21:22:31 -0300

    Fixing code structure

--- front-end/login-register-form/src/App.js
+++ front-end/login-register-form/src/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
-import { Login } from './login';
-import { Register } from './register';
+import { Login } from './components/login';
+import { Register } from './components/register';
 
 function App() {
   const [currentForm, setCurrentForm] = useState('login')
